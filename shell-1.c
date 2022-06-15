6f0cabe54a2bc2796f99dae1d674dd753e33067c
walebign3 <walebign3@gmail.com>
2022-06-15 13:33:27 +0300

    shell-1.c

---
+++ shell-1.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - super simple shell
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	pid_t ch;
+	char *tok, *lnptr = NULL;
+	size_t i, n;
+	char **cmd = malloc(64 * sizeof(char*));
+
+	if (!cmd)
+	{
+		perror("lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	if (argc != 1)
+		perror("./shell: No such file or directory");
+	while(1)
+	{
+		write(STDOUT_FILENO,"#cisfun$ ",10);
+		if (getline(&lnptr, &n, stdin) == -1)
+		{
+			exit(EXIT_FAILURE);
+			break;
+		}
+		tok = strtok(lnptr, " \t\n\r");
+		for (i = 0; i < 64 && tok != NULL; i++)
+		{
+			cmd[i] = tok;
+			tok = strtok(NULL, " \n\t\r");
+		}
+		cmd[i] = NULL;
+		ch = fork();
+		if (ch == 0)
+		{
+			if (execve(cmd[0], cmd, NULL))
+			{
+				perror(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		if (ch > 0)
+		wait(NULL);
+	}
+	write(STDOUT_FILENO,"\n",1);
+	free(cmd);
+	free(lnptr);
+	return 0;
+}

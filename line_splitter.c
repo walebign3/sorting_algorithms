77087141de60e657f210659c6b6a4cc5a1e57aed
walebign3 <walebign3@gmail.com>
2022-06-16 10:03:47 +0300

    Simple shell 0.1

--- line_splitter.c
+++ line_splitter.c
@@ -21,16 +21,6 @@ char **tokens_fun(char *lnptr)
 	{
 		tokens[i] = token;
 		i++;
-		if (i >= bufsize)
-		{
-			bufsize += BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens)
-			{
-				write(STDOUT_FILENO,"mem alloc error\n",16);
-				exit(EXIT_FAILURE);
-			}
-		}
 		token = strtok(NULL, DELIM);
 	}
 	tokens[i] = NULL;

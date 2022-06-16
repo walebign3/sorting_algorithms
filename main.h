8ff9d0fb142022e410549b2c01f038fd998805cb
walebign3 <walebign3@gmail.com>
2022-06-16 18:38:12 +0300

    shell02

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFSIZE 64
+#define DELIM " \t\r\n\a"
+
+char **tokens_fun(char *lnptr);
+void execute_fun(char **tok, char **argv);
+void loop_fun(char **argv);
+void handle_signal(int m);
+
+#endif /* _HEADER_H_ */

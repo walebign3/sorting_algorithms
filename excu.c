c8c8edc271881215e8f9f2d816b5294a2bd4c8e9
walebign3 <walebign3@gmail.com>
2022-06-19 11:25:16 +0300

    shell_01+

---
+++ excu.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * execute_fun - split command arguments
+ * @tok: pointer to command arguments
+ *
+ * Return: line of pointer.
+ */
+void execute_fun(char **tok, char **argv)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(tok[0], tok, NULL) == -1)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("fork() error");
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+

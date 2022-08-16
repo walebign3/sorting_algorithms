fe81503bd7b5998fbfd504030dd1a420a260c47e
walebign3 <walebign3@gmail.com>
2022-08-16 23:03:49 +0300

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i <= size - 2; i++)
 	{
 		position = i;
-		for (j = i + 1; j < size; j++)
+		for (j = (i + 1); j < size; j++)
 		{
 			if (array[j] < array[position])
 				position = j;
